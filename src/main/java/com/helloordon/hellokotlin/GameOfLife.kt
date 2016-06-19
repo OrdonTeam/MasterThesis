ac473a895ce1831aaeaa833d0372793830d95f98
Karol Kowalski <ordon1313@gmail.com>
2016-06-19 20:19:02 +0200

    Init

---
+++ src/main/java/com/helloordon/hellokotlin/GameOfLife.kt
@@ -0,0 +1,4 @@
+package com.helloordon.hellokotlin
+
+class GameOfLife {
+}
---
+++ src/test/java/com/helloordon/hellokotlin/GameOfLifeTest.kt
@@ -0,0 +1,12 @@
+package com.helloordon.hellokotlin
+
+import org.junit.Assert
+import org.junit.Test
+
+class GameOfLifeTest {
+
+    @Test
+    fun shouldPass() {
+        Assert.assertTrue(true)
+    }
+}
