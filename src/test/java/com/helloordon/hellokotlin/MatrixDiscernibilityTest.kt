3903e963b6270d2fcb1dd1835e66d0242d7ab54e
Karol Kowalski <ordon1313@gmail.com>
2016-09-21 23:18:58 +0200

    Find matrix discernibility for multiple rows

--- src/test/java/com/helloordon/hellokotlin/MatrixDiscernibilityTest.kt
+++ src/test/java/com/helloordon/hellokotlin/MatrixDiscernibilityTest.kt
@@ -21,11 +21,23 @@ class MatrixDiscernibilityTest {
                 listOf(listOf(1)))
     }
 
+    @Test
+    fun shouldFindMatrixDiscernibilityForMultipleRows() {
+        assertMatrixDiscernibility(
+                listOf(listOf(true, false), listOf(false, true)),
+                listOf(listOf(false, false)),
+                listOf(listOf(0), listOf(1)))
+    }
+
     private fun assertMatrixDiscernibility(a: List<List<Boolean>>, b: List<List<Boolean>>, discernibility: List<List<Int>>) {
         Assert.assertEquals(discernibility, findMatrixDiscernibility(a, b))
     }
 
     private fun findMatrixDiscernibility(groupA: List<List<Boolean>>, groupB: List<List<Boolean>>): List<List<Int>> {
-        return listOf(findDiscernibility(groupA[0], groupB[0]))
+        return groupA.flatMap { vectorA ->
+            groupB.map { vectorB ->
+                findDiscernibility(vectorA, vectorB)
+            }
+        }
     }
 }
