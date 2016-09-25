e822678b4b6ce67c6f499bcf0150335127562b1e
Karol Kowalski <ordon1313@gmail.com>
2016-09-25 20:53:33 +0200

    Move saveMissingPairsToFile to upper level

---
+++ src/main/java/com/helloordon/hellokotlin/Write.kt
@@ -0,0 +1,14 @@
+package com.helloordon.hellokotlin
+
+import java.io.File
+
+fun saveMissingPairsToFile(file: File, argumentsCount: Int, pairs: List<List<Int>>) {
+    file.writer().use {
+        pairs.map { pair ->
+            val otherArguments = (0..(argumentsCount - 1)).filterNot { it == pair[0] || it == pair[1] }.map { " + x$it" }.joinToString()
+            "x${pair[0]} * x${pair[1]}" + otherArguments
+        }.forEach { line ->
+            it.appendln(line)
+        }
+    }
+}
