3082a6e91af4cb6f2e67706854e0dc2326ed6165
Karol Kowalski <ordon1313@gmail.com>
2016-09-25 20:00:00 +0200

    Find one separate pair

--- src/test/java/com/helloordon/hellokotlin/SeparatePairsTest.kt
+++ src/test/java/com/helloordon/hellokotlin/SeparatePairsTest.kt
@@ -10,12 +10,20 @@ class SeparatePairsTest {
         verify(emptyList(), listOf(listOf(0, 1)))
     }
 
-    private fun verify(expected: List<List<Int>>, collections: List<List<Int>>) {
-        Assert.assertEquals(expected, getSeparatePairs(collections))
+    @Test
+    fun shouldFindOneSeparatePair() {
+        verify(listOf(listOf(0, 1) to listOf(2, 3)), listOf(listOf(0, 1), listOf(2, 3)))
     }
 
+    private fun verify(expected: List<Pair<List<Int>, List<Int>>>, collections: List<List<Int>>) {
+        Assert.assertEquals(expected, getSeparatePairs(collections))
+    }
 }
 
-fun getSeparatePairs(collections: List<List<Int>>): List<List<Int>> {
-    return emptyList()
+fun getSeparatePairs(collections: List<List<Int>>): List<Pair<List<Int>, List<Int>>> {
+    return (0..(collections.size - 1)).flatMap { first ->
+        ((first + 1)..collections.size - 1).map { second ->
+            collections[first] to collections[second]
+        }
+    }
 }
