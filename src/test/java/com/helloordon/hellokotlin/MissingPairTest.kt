5dc9018b1531972a5005ec7da1b5e021690ee99c
Karol Kowalski <ordon1313@gmail.com>
2016-09-25 15:18:17 +0200

    Find one missing pair

--- src/test/java/com/helloordon/hellokotlin/MissingPairTest.kt
+++ src/test/java/com/helloordon/hellokotlin/MissingPairTest.kt
@@ -10,12 +10,24 @@ class MissingPairTest {
         verifyFoundPairs(emptyList(), 2, listOf(listOf(0, 1)))
     }
 
-    private fun verifyFoundPairs(expected: List<Int>, argumentsCount: Int, pairs: List<List<Int>>) {
+    @Test
+    fun shouldFindOneMissingPair() {
+        verifyFoundPairs(listOf(listOf(0, 1)), 2, emptyList())
+    }
+
+    private fun verifyFoundPairs(expected: List<List<Int>>, argumentsCount: Int, pairs: List<List<Int>>) {
         Assert.assertEquals(expected, getMissingPairs(argumentsCount, pairs))
     }
 
 }
 
-fun getMissingPairs(argumentsCount: Int, pairs: List<List<Int>>): List<Int> {
-    return emptyList()
+fun getMissingPairs(argumentsCount: Int, pairs: List<List<Int>>): List<List<Int>> {
+    val list = (0..0).flatMap { first ->
+        (1..1).map { second ->
+            listOf(first, second)
+        }
+    }
+    return list.filter {
+        !pairs.contains(it)
+    }
 }
