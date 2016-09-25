e550de1ca94556d97259457caa0fe83e1b2734b3
Karol Kowalski <ordon1313@gmail.com>
2016-09-25 21:02:36 +0200

    Save missing pairs to file

--- src/test/java/com/helloordon/hellokotlin/ExampleFunctionTest.kt
+++ src/test/java/com/helloordon/hellokotlin/ExampleFunctionTest.kt
@@ -1,10 +1,15 @@
 package com.helloordon.hellokotlin
 
 import com.helloordon.hellokotlin.utils.fileFromResources
+import org.junit.After
 import org.junit.Assert
 import org.junit.Test
+import java.io.File
+import java.util.*
 
 class ExampleFunctionTest {
+
+    val file = File(UUID.randomUUID().toString())
 
     @Test
     fun shouldCalculateCpq() {
@@ -68,6 +73,28 @@ class ExampleFunctionTest {
                         getMissingPairs(
                                 zeroRows.first().size,
                                 findMatrixDiscernibility(zeroRows, oneRows))))
+    }
+
+    @Test
+    fun shouldSaveMissingPairsToFile() {
+        val function = readFunctionFromFile(fileFromResources("example_function"))
+        val zeroRows = function[false]!!
+        val oneRows = function[true]!!
+        saveMissingPairsToFile(file, zeroRows.first().size,
+                getMissingPairs(
+                        zeroRows.first().size,
+                        findMatrixDiscernibility(zeroRows, oneRows)))
+        Assert.assertEquals(
+                listOf(
+                        "x0 * x4 + x1 + x2 + x3",
+                        "x2 * x3 + x0 + x1 + x4",
+                        "x2 * x4 + x0 + x1 + x3"),
+                file.readLines())
+    }
+
+    @After
+    fun tearDown() {
+        file.delete()
     }
 
     private fun List<List<Int>>.toBoolean() = map { it.map { if (it == 0) false else true } }
