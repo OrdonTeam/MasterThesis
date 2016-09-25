3122da35641bcce1e9c0abcc3adc4c8d45136c80
Karol Kowalski <ordon1313@gmail.com>
2016-09-25 18:14:57 +0200

    Parse single input function

--- src/test/java/com/helloordon/hellokotlin/ParseTest.kt
+++ src/test/java/com/helloordon/hellokotlin/ParseTest.kt
@@ -3,11 +3,17 @@ package com.helloordon.hellokotlin
 import com.helloordon.hellokotlin.utils.fileFromResources
 import org.junit.Assert
 import org.junit.Test
+import java.io.File
 
 class ParseTest {
 
     @Test
     fun shouldParseSingleInputFunction() {
-        Assert.assertTrue(fileFromResources("singleInputFile").exists())
+        Assert.assertEquals(listOf(listOf(false), listOf(true)), readFunctionFromFile(fileFromResources("singleInputFile")))
     }
+
+}
+
+private fun readFunctionFromFile(file: File): List<List<Boolean>> {
+    return listOf(listOf(false), listOf(true))
 }
