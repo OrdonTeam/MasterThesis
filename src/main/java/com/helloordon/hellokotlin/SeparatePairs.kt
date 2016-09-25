5559e42fd278c17a92c6aa631ebfb7eb20dce06e
Karol Kowalski <ordon1313@gmail.com>
2016-09-25 20:06:49 +0200

    Move getSeparatePairs to upper level

---
+++ src/main/java/com/helloordon/hellokotlin/SeparatePairs.kt
@@ -0,0 +1,13 @@
+package com.helloordon.hellokotlin
+
+fun getSeparatePairs(collections: List<List<Int>>): List<Pair<List<Int>, List<Int>>> {
+    return (0..(collections.size - 1)).flatMap { first ->
+        ((first + 1)..collections.size - 1).map { second ->
+            collections[first] to collections[second]
+        }.filter { pair ->
+            pair.first.all {
+                !pair.second.contains(it)
+            }
+        }
+    }
+}
--- src/test/java/com/helloordon/hellokotlin/SeparatePairsTest.kt
+++ src/test/java/com/helloordon/hellokotlin/SeparatePairsTest.kt
@@ -24,15 +24,3 @@ class SeparatePairsTest {
         Assert.assertEquals(expected, getSeparatePairs(collections))
     }
 }
-
-fun getSeparatePairs(collections: List<List<Int>>): List<Pair<List<Int>, List<Int>>> {
-    return (0..(collections.size - 1)).flatMap { first ->
-        ((first + 1)..collections.size - 1).map { second ->
-            collections[first] to collections[second]
-        }.filter { pair ->
-            pair.first.all {
-                !pair.second.contains(it)
-            }
-        }
-    }
-}
