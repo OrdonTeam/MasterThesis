30c344205cced400cb6300cc1b7c1b55d6687e04
Karol Kowalski <ordon1313@gmail.com>
2016-09-25 20:51:36 +0200

    Save missing pairs

--- src/test/java/com/helloordon/hellokotlin/WriteTest.kt
+++ src/test/java/com/helloordon/hellokotlin/WriteTest.kt
@@ -13,7 +13,13 @@ class WriteTest {
     @Test
     fun shouldSaveMissingPair() {
         saveMissingPairsToFile(file, 3, listOf(listOf(0, 1)))
-        Assert.assertEquals(file.readLines(), listOf("x0 * x1 + x2"))
+        Assert.assertEquals(listOf("x0 * x1 + x2"), file.readLines())
+    }
+
+    @Test
+    fun shouldSaveMissingPairs() {
+        saveMissingPairsToFile(file, 3, listOf(listOf(0, 1), listOf(0, 2)))
+        Assert.assertEquals(listOf("x0 * x1 + x2", "x0 * x2 + x1"), file.readLines())
     }
 
     @After
@@ -23,8 +29,12 @@ class WriteTest {
 }
 
 private fun saveMissingPairsToFile(file: File, argumentsCount: Int, pairs: List<List<Int>>) {
-    file.writer().run {
-        appendln("x0 * x1 + x2")
-        flush()
+    file.writer().use {
+        pairs.map { pair ->
+            val otherArguments = (0..(argumentsCount - 1)).filterNot { it == pair[0] || it == pair[1] }.map { " + x$it" }.joinToString()
+            "x${pair[0]} * x${pair[1]}" + otherArguments
+        }.forEach { line ->
+            it.appendln(line)
+        }
     }
 }
