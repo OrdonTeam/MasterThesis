5d123ac00aef38fddaa5d6ecca86b70d46faf954
Karol Kowalski <ordon1313@gmail.com>
2016-10-05 22:36:11 +0200

    Flip condition

--- src/main/java/com/helloordon/hellokotlin/algorithm/MissingPairs.kt
+++ src/main/java/com/helloordon/hellokotlin/algorithm/MissingPairs.kt
@@ -4,7 +4,7 @@ import com.helloordon.hellokotlin.util.allPairs
 
 fun getMissingPairs(argumentsCount: Int, pairs: List<List<Int>>): List<List<Int>> {
     val list = (0 until argumentsCount).allPairs().map { listOf(it.first, it.second) }
-    return list.filter {
-        !pairs.contains(it)
+    return list.filterNot {
+        pairs.contains(it)
     }
 }
