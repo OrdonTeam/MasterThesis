30daf01f5330b1b18583d2911ebaffd0fd301ad9
Karol Kowalski <ordon1313@gmail.com>
2016-10-05 20:34:17 +0200

    Save missing fours

--- src/test/java/com/helloordon/hellokotlin/write/WriteMissingFoursTest.kt
+++ src/test/java/com/helloordon/hellokotlin/write/WriteMissingFoursTest.kt
@@ -16,9 +16,20 @@ class WriteMissingFoursTest {
         Assert.assertEquals(listOf("(x0 * x1) * (x2 * x3)"), file.readLines())
     }
 
+    @Test
+    fun shouldSaveMissingFours() {
+        writeMissingFours(file, 5, listOf(listOf(0, 1) to listOf(2, 3), listOf(1, 2) to listOf(3, 4)))
+        Assert.assertEquals(listOf("(x0 * x1) * (x2 * x3) + x4", "(x1 * x2) * (x3 * x4) + x0"), file.readLines())
+    }
+
     private fun writeMissingFours(file: File, argumentsCount: Int, fours: List<Pair<List<Int>, List<Int>>>) {
         file.writer().use {
-            it.appendln("(x0 * x1) * (x2 * x3)")
+            fours.map { four ->
+                val otherArguments = (0..(argumentsCount - 1)).filterNot { (four.first + four.second).contains(it) }.map { " + x$it" }.joinToString("")
+                "(x${four.first[0]} * x${four.first[1]}) * (x${four.second[0]} * x${four.second[1]})" + otherArguments
+            }.forEach { line ->
+                it.appendln(line)
+            }
         }
     }
 
