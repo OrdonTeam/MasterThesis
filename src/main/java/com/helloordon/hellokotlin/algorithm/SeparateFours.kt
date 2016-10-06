b0ef86c29e78df5092319cb7ec4e87b04032bda9
Karol Kowalski <ordon1313@gmail.com>
2016-10-06 22:19:23 +0200

    Find one separate pair of fours

--- src/main/java/com/helloordon/hellokotlin/algorithm/SeparateFours.kt
+++ src/main/java/com/helloordon/hellokotlin/algorithm/SeparateFours.kt
@@ -1,5 +1,7 @@
 package com.helloordon.hellokotlin.algorithm
 
+import com.helloordon.hellokotlin.util.allPairs
+
 fun getSeparateFours(collections: List<Pair<List<Int>, List<Int>>>): List<Pair<Pair<List<Int>, List<Int>>, Pair<List<Int>, List<Int>>>> {
-    return emptyList()
+    return collections.allPairs()
 }
--- src/test/java/com/helloordon/hellokotlin/algorithm/SeparateFoursTest.kt
+++ src/test/java/com/helloordon/hellokotlin/algorithm/SeparateFoursTest.kt
@@ -7,7 +7,18 @@ class SeparateFoursTest {
 
     @Test
     fun shouldFindNoSeparateFours() {
-        verify(emptyList(), listOf(listOf(0, 1) to listOf(2, 3)))
+        verify(
+                emptyList(),
+                listOf(listOf(0, 1) to listOf(2, 3)))
+    }
+
+    @Test
+    fun shouldFindOnePairOfFours() {
+        val one = listOf(0, 1) to listOf(2, 3)
+        val two = listOf(4, 5) to listOf(6, 7)
+        verify(
+                listOf(one to two),
+                listOf(one, two))
     }
 
     private fun verify(expected: List<Pair<Pair<List<Int>, List<Int>>, Pair<List<Int>, List<Int>>>>, collections: List<Pair<List<Int>, List<Int>>>) {
