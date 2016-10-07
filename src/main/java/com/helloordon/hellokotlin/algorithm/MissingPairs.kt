2117f856622aea83899c97db65b7274222903815
Karol Kowalski <ordon1313@gmail.com>
2016-10-07 20:49:07 +0200

    Extract allPairs

--- src/main/java/com/helloordon/hellokotlin/algorithm/MissingPairs.kt
+++ src/main/java/com/helloordon/hellokotlin/algorithm/MissingPairs.kt
@@ -8,12 +8,16 @@ fun List<List<Int>>.toMissingPairs(argumentsCount: Int): Observable<Argument> {
 }
 
 private fun getMissingPairs(argumentsCount: Int, discernibility: List<List<Int>>): Observable<Argument> {
+    return allPairs(argumentsCount).filter {
+        !discernibility.contains(it.asList().sorted())
+    }
+}
+
+private fun allPairs(argumentsCount: Int): Observable<Argument> {
     return Observable.create({ source ->
         (0 until argumentsCount).forEach { first ->
             ((first + 1) until argumentsCount).forEach { second ->
-                val pair = Argument.Single(first) with Argument.Single(second)
-                if (!discernibility.contains(pair.asList()))
-                    source.onNext(pair)
+                source.onNext(Argument.Single(first) with Argument.Single(second))
             }
         }
         source.onComplete()
