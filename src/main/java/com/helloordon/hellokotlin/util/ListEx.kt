a5633e69970b24b79b4ac622cef8910c58b45ba1
Karol Kowalski <ordon1313@gmail.com>
2016-10-07 19:24:54 +0200

    Remove allPairs from ListEx

--- src/main/java/com/helloordon/hellokotlin/util/ListEx.kt
+++ src/main/java/com/helloordon/hellokotlin/util/ListEx.kt
@@ -1,17 +1,5 @@
 package com.helloordon.hellokotlin.util
 
-fun <E> Iterable<E>.allPairs(): List<Pair<E, E>> {
-    return this.toList().allPairs()
-}
-
-fun <E> List<E>.allPairs(): List<Pair<E, E>> {
-    return (0 until size).flatMap { first ->
-        ((first + 1) until size).map { second ->
-            this[first] to this[second]
-        }
-    }
-}
-
 infix fun List<Int>.isDisjointWith(other: List<Int>): Boolean {
     return all { !other.contains(it) }
 }
