cfd37b6450c1e0acea853588e6667af4b764e61d
Karol Kowalski <ordon1313@gmail.com>
2016-10-07 17:55:43 +0200

    Refactor Main

--- src/main/java/com/helloordon/hellokotlin/Main.kt
+++ src/main/java/com/helloordon/hellokotlin/Main.kt
@@ -22,17 +22,17 @@ object Main {
         val function = readFunction(File(args[0]))
         val zeroRows = function[false]!!
         val oneRows = function[true]!!
-
         val discernibility = findMatrixDiscernibility(zeroRows, oneRows)
-        val missingPairs = getMissingPairs(zeroRows.first().size, discernibility)
-        writeMissingPairs(File(args[1]).appendingOutputStream(), zeroRows.first().size, missingPairs.toList().blockingGet())
 
-        val separatePairs = missingPairs.toSeparatePairs()
-        val missingFours =  separatePairs.toMissingFours(discernibility)
-        writeMissingFours(File(args[1]).appendingOutputStream(), zeroRows.first().size, missingFours.toList().blockingGet())
-
-        val separateFours = missingFours.toSeparateFours()
-        val missingEights = separateFours.toMissingEights(discernibility)
-        writeMissingEights(File(args[1]).appendingOutputStream(), zeroRows.first().size, missingEights.toList().blockingGet())
+        getMissingPairs(zeroRows.first().size, discernibility)
+                .doOnNext { writeMissingPairs(File(args[1]).appendingOutputStream(), zeroRows.first().size, listOf(it)) }
+                .toSeparatePairs()
+                .toMissingFours(discernibility)
+                .doOnNext { writeMissingFours(File(args[1]).appendingOutputStream(), zeroRows.first().size, listOf(it)) }
+                .toSeparateFours()
+                .toMissingEights(discernibility)
+                .doOnNext { writeMissingEights(File(args[1]).appendingOutputStream(), zeroRows.first().size, listOf(it)) }
+                .take(1)
+                .subscribe()
     }
 }
