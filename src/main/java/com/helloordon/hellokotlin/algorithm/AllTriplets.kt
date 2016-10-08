4a2a32b8c035074eab4e2d130c6523e17693320a
Karol Kowalski <ordon1313@gmail.com>
2016-10-08 21:45:06 +0200

    Multiple triplets

--- src/main/java/com/helloordon/hellokotlin/algorithm/AllTriplets.kt
+++ src/main/java/com/helloordon/hellokotlin/algorithm/AllTriplets.kt
@@ -3,11 +3,13 @@ package com.helloordon.hellokotlin.algorithm
 import com.helloordon.hellokotlin.dto.Argument
 import io.reactivex.Observable
 
-fun allTriplets(argumentsCount: Int) : Observable<Argument> {
+fun allTriplets(argumentsCount: Int): Observable<Argument> {
     return Observable.create({ source ->
         (0 until argumentsCount).forEach { first ->
-            ((first + 1) until argumentsCount-1).forEach { second ->
-                source.onNext(Argument.Single(first) with Argument.Single(second))
+            ((first + 1) until argumentsCount).forEach { second ->
+                ((second + 1) until argumentsCount).forEach { third ->
+                    source.onNext(Argument.Single(first) with Argument.Single(second))
+                }
             }
         }
         source.onComplete()
--- src/test/java/com/helloordon/hellokotlin/algorithm/AllTripletsTest.kt
+++ src/test/java/com/helloordon/hellokotlin/algorithm/AllTripletsTest.kt
@@ -17,9 +17,24 @@ class AllTripletsTest {
         verify(listOf(triplet(0, 1, 2)), 3)
     }
 
+    @Test
+    fun shouldReturnFourTriplets() {
+        verify(listOf(triplet(0, 1, 2), triplet(0, 1, 3), triplet(0, 2, 3), triplet(1, 2, 3)), 4)
+    }
+
+    @Test
+    fun shouldReturnTripletsFor5() {
+        verify(listOf(
+                triplet(0, 1, 2), triplet(0, 1, 3), triplet(0, 1, 4), triplet(0, 2, 3), triplet(0, 2, 4),triplet(0, 3, 4),
+                triplet(1, 2, 3), triplet(1, 2, 4), triplet(1, 3, 4),
+                triplet(2, 3, 4)), 5)
+
+    }
+
     private fun verify(expected: List<Argument>, argumentsCount: Int) {
         allTriplets(argumentsCount)
                 .test()
-                .assertValueSequence(expected)
+                .assertValueSet(expected)
+                .assertValueCount(expected.size)
     }
 }
