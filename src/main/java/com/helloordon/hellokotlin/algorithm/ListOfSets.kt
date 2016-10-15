0421805e4caba1083102226bd7156edf36446f0c
Karol Kowalski <ordon1313@gmail.com>
2016-10-15 23:59:30 +0200

    Refactoring list of sets renames

--- src/main/java/com/helloordon/hellokotlin/algorithm/ListOfSets.kt
+++ src/main/java/com/helloordon/hellokotlin/algorithm/ListOfSets.kt
@@ -3,41 +3,43 @@ package com.helloordon.hellokotlin.algorithm
 import io.reactivex.Observable
 import io.reactivex.ObservableEmitter
 
-fun listOfSets(n: Int): Observable<List<Int>> {
+fun listOfSets(setSize: Int): Observable<List<Int>> {
     return Observable.create { source ->
-        (2..n).forEach {
-            mapToListOfSets(it, n, source).get()
+        (2..setSize).forEach { subSetSize ->
+            createSetGenerator(subSetSize, setSize, source).generate()
         }
         source.onComplete()
     }
 }
 
-private fun mapToListOfSets(nestedLoopCount: Int, n: Int, source: ObservableEmitter<List<Int>>): Action {
+private fun createSetGenerator(nestedLoopCount: Int, n: Int, source: ObservableEmitter<List<Int>>): Generator {
     return if (nestedLoopCount < 1) {
-        Action.Consumer(source)
+        Generator.Consumer(source)
     } else {
-        Action.Wrapper(n, mapToListOfSets(nestedLoopCount - 1, n, source))
+        Generator.Wrapper(n, createSetGenerator(nestedLoopCount - 1, n, source))
     }
 }
 
-private sealed class Action : (Int, List<Int>) -> Unit {
+private sealed class Generator {
 
-    class Consumer(val source: ObservableEmitter<List<Int>>) : Action() {
-        override fun invoke(from: Int, list: List<Int>) {
-            source.onNext(list)
+    protected abstract fun appendSet(from: Int, setToAppend: List<Int>)
+
+    class Consumer(val source: ObservableEmitter<List<Int>>) : Generator() {
+        override fun appendSet(from: Int, setToAppend: List<Int>) {
+            source.onNext(setToAppend)
         }
     }
 
-    class Wrapper(val to: Int, val action: Action) : Action() {
-        override fun invoke(from: Int, list: List<Int>) {
+    class Wrapper(val to: Int, val generator: Generator) : Generator() {
+        override fun appendSet(from: Int, setToAppend: List<Int>) {
             (from until to).forEach {
-                action(it + 1, list + it)
+                generator.appendSet(it + 1, setToAppend + it)
             }
         }
     }
 
-    fun get(): Unit {
-        invoke(0, emptyList())
+    fun generate(): Unit {
+        appendSet(0, emptyList())
     }
 }
 
