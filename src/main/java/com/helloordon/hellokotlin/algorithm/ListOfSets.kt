c4e6ba9788854d5de64f393a322f40e047f4fa55
Karol Kowalski <ordon1313@gmail.com>
2016-10-15 23:46:51 +0200

    Refactoring list of sets change ranges to int

--- src/main/java/com/helloordon/hellokotlin/algorithm/ListOfSets.kt
+++ src/main/java/com/helloordon/hellokotlin/algorithm/ListOfSets.kt
@@ -5,22 +5,18 @@ import io.reactivex.ObservableEmitter
 
 fun listOfSets(n: Int): Observable<List<Int>> {
     return Observable.create { source ->
-        listOfRanges(n).forEach {
+        (1 until n).forEach {
             mapToListOfSets(it, n, source).get()
         }
         source.onComplete()
     }
 }
 
-private fun listOfRanges(n: Int): List<List<Int>> {
-    return (1 until n).map { (0..it).toList() }
-}
-
-private fun mapToListOfSets(range: List<Int>, n: Int, source: ObservableEmitter<List<Int>>): Action {
-    return if (range.isEmpty()) {
+private fun mapToListOfSets(endOffset: Int, n: Int, source: ObservableEmitter<List<Int>>): Action {
+    return if (endOffset < 0) {
         Action.Consumer(source)
     } else {
-        Action.Wrapper(n - range.last(), mapToListOfSets(range.dropLast(1), n, source))
+        Action.Wrapper(n - endOffset, mapToListOfSets(endOffset - 1, n, source))
     }
 }
 
