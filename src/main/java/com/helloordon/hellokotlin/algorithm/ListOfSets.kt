240802ae1264cbd937175e34f573d4b68a1fb740
Karol Kowalski <ordon1313@gmail.com>
2016-10-15 23:37:27 +0200

    Refactoring list of sets imperative wrapping

--- src/main/java/com/helloordon/hellokotlin/algorithm/ListOfSets.kt
+++ src/main/java/com/helloordon/hellokotlin/algorithm/ListOfSets.kt
@@ -6,7 +6,7 @@ import io.reactivex.ObservableEmitter
 fun listOfSets(n: Int): Observable<List<Int>> {
     return Observable.create { source ->
         listOfRanges(n).forEach {
-            mapToListOfSets(it, n, source)
+            mapToListOfSets(it, n, source).get()
         }
         source.onComplete()
     }
@@ -16,29 +16,32 @@ private fun listOfRanges(n: Int): List<IntRange> {
     return (1 until n).map { 0..it }
 }
 
-private fun mapToListOfSets(range: IntRange, n: Int, source: ObservableEmitter<List<Int>>) {
-    range.fold(
-            Consumer(source) as Action,
-            { action, i -> Wrapper(n - i, action) }
-    ).get()
-}
-
-private fun ((Int, List<Int>) -> Unit).get(): Unit {
-    return this.invoke(0, emptyList())
-}
-
-private interface Action : (Int, List<Int>) -> Unit
-
-private class Consumer(val source: ObservableEmitter<List<Int>>) : Action {
-    override fun invoke(from: Int, list: List<Int>) {
-        source.onNext(list)
+private fun mapToListOfSets(range: IntRange, n: Int, source: ObservableEmitter<List<Int>>): Action {
+    var action: Action = Action.Consumer(source)
+    for (i in range) {
+        action = Action.Wrapper(n - i, action)
     }
+    return action
 }
 
-private class Wrapper(val to: Int, val action: Action) : Action {
-    override fun invoke(from: Int, list: List<Int>) {
-        (from until to).forEach {
-            action(it + 1, list + it)
+private sealed class Action : (Int, List<Int>) -> Unit {
+
+    class Consumer(val source: ObservableEmitter<List<Int>>) : Action() {
+        override fun invoke(from: Int, list: List<Int>) {
+            source.onNext(list)
         }
     }
+
+    class Wrapper(val to: Int, val action: Action) : Action() {
+        override fun invoke(from: Int, list: List<Int>) {
+            (from until to).forEach {
+                action(it + 1, list + it)
+            }
+        }
+    }
+
+    fun get(): Unit {
+        invoke(0, emptyList())
+    }
 }
+
