b269a33437c31e84c0758a04a92401c47d2e89d2
Karol Kowalski <ordon1313@gmail.com>
2016-10-16 17:20:42 +0200

    Get argument count from steam

--- src/main/java/com/helloordon/hellokotlin/read/ReadFunction.kt
+++ src/main/java/com/helloordon/hellokotlin/read/ReadFunction.kt
@@ -4,13 +4,17 @@ import java.io.File
 
 fun readFunction(file: File): Map<String, List<List<Boolean>>> {
     val readFile = file.readLines().groupBy { it.startsWith(".") }
-    val argumentCount = getArgumentCount(readFile)
+    val argumentCount = getArgumentCount(file)
     return parseFunction(argumentCount, readFile)
 }
 
-private fun parseFunction(argumentCount: Int, readFile: Map<Boolean, List<String>>) = readFile[false]!!.map { parseLine(it, argumentCount) }.groupBy({ it.first }, { it.second })
+fun getArgumentCount(file: File) : Int {
+    return file.reader().useLines {
+        it.first { it.startsWith(".i") }.drop(3).trim().toInt()
+    }
+}
 
-private fun getArgumentCount(readFile: Map<Boolean, List<String>>) = readFile[true]!!.first { it.startsWith(".i") }.split(" ")[1].toInt()
+private fun parseFunction(argumentCount: Int, readFile: Map<Boolean, List<String>>) = readFile[false]!!.map { parseLine(it, argumentCount) }.groupBy({ it.first }, { it.second })
 
 private fun parseLine(line: String, argumentCount: Int): Pair<String, List<Boolean>> {
     val noSpaces = line.replace(" ", "")
