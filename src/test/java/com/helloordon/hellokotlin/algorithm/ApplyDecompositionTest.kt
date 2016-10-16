d8942644a24daac44b0b214119ffa8e671797516
Karol Kowalski <ordon1313@gmail.com>
2016-10-16 20:19:07 +0200

    Promote apply decomposition to file

--- src/test/java/com/helloordon/hellokotlin/algorithm/ApplyDecompositionTest.kt
+++ src/test/java/com/helloordon/hellokotlin/algorithm/ApplyDecompositionTest.kt
@@ -57,21 +57,3 @@ class ApplyDecompositionTest {
     }
 }
 
-private fun BooleanFunction.applyDecomposition(decomposition: List<Int>): BooleanFunction {
-    return BooleanFunction(data.map { it.applyDecomposition(decomposition) })
-}
-
-private fun BooleanFunctionRow.applyDecomposition(decomposition: List<Int>): BooleanFunctionRow {
-    return copy(arguments.applyDecomposition(decomposition))
-}
-
-private fun List<Boolean>.applyDecomposition(decomposition: List<Int>): List<Boolean> {
-    return argumentsNotInDecomposition(decomposition) + argumentsInDecomposition(decomposition)
-}
-
-private fun List<Boolean>.argumentsNotInDecomposition(decomposition: List<Int>) = filterIndexed { i, b -> !decomposition.contains(i) }
-
-private fun List<Boolean>.argumentsInDecomposition(decomposition: List<Int>): List<Boolean> {
-    val arguments = filterIndexed { i, b -> decomposition.contains(i) }
-    return arguments.dropLast(1).zip(arguments.drop(1)).map { it.first.xor(it.second) }
-}
