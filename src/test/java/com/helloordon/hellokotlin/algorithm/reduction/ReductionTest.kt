12dce12e4c5d82090beaa0d58ed0f91b830cd0a9
Karol Kowalski <ordon1313@gmail.com>
2016-10-22 23:35:17 +0200

    Find best argument to reduction - final

--- src/test/java/com/helloordon/hellokotlin/algorithm/reduction/ReductionTest.kt
+++ src/test/java/com/helloordon/hellokotlin/algorithm/reduction/ReductionTest.kt
@@ -7,17 +7,20 @@ class ReductionTest {
 
     @Test
     fun shouldFindBestArgumentToReduction() {
-        verifyBestArgument(0, listOf(listOf(0)), 1)
-        verifyBestArgument(1, listOf(listOf(1)), 2)
-        verifyBestArgument(0, listOf(listOf(1), listOf(0), listOf(0)), 2)
+        verifyBestArgument(0, listOf(listOf(0)))
+        verifyBestArgument(1, listOf(listOf(1)))
+        verifyBestArgument(0, listOf(listOf(1), listOf(0), listOf(0)))
+        verifyBestArgument(2, listOf(listOf(0, 1, 2), listOf(0, 1), listOf(2)))
     }
 
-    private fun verifyBestArgument(expected: Int, discernibility: List<List<Int>>, attributesCount: Int) {
-        Assert.assertEquals(expected, findBestArgumentToReduction(discernibility, attributesCount))
+    private fun verifyBestArgument(expected: Int, discernibility: List<List<Int>>) {
+        Assert.assertEquals(expected, findBestArgumentToReduction(discernibility))
     }
 
-    private fun findBestArgumentToReduction(discernibility: List<List<Int>>, attributesCount: Int): Int {
-        val count = discernibility.flatMap { it }.groupBy { it }.mapValues { it.value.count() }
-        return count.maxBy { it.value }!!.key
+    private fun findBestArgumentToReduction(discernibility: List<List<Int>>): Int {
+        val minDiscernibilityLength = discernibility.minBy { it.count() }!!.count()
+        val candidates = discernibility.filter { it.count() == minDiscernibilityLength }.flatMap { it }
+        val countInColumns = discernibility.flatMap { it }.groupBy { it }.mapValues { it.value.count() }
+        return countInColumns.filter { candidates.contains(it.key) }.maxBy { it.value }!!.key
     }
 }
