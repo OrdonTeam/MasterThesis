f5a35d06dfe39ebdf02f66705bf81c39adf1ef14
Karol Kowalski <ordon1313@gmail.com>
2016-10-23 14:15:40 +0200

    removeRowsFromDiscernibility

--- src/test/java/com/helloordon/hellokotlin/algorithm/reduction/ReductionTest.kt
+++ src/test/java/com/helloordon/hellokotlin/algorithm/reduction/ReductionTest.kt
@@ -13,6 +13,12 @@ class ReductionTest {
         verifyBestArgument(2, listOf(listOf(0, 1, 2), listOf(0, 1), listOf(2)))
     }
 
+    @Test
+    fun shouldRemoveRowsByArgument() {
+        Assert.assertEquals(emptyList<Int>(), removeRowsFromDiscernibility(listOf(listOf(0)),0))
+        Assert.assertEquals(listOf(listOf(0)), removeRowsFromDiscernibility(listOf(listOf(0)),1))
+    }
+
     private fun verifyBestArgument(expected: Int, discernibility: List<List<Int>>) {
         Assert.assertEquals(expected, findBestArgumentToReduction(discernibility))
     }
@@ -22,5 +28,9 @@ class ReductionTest {
         val candidates = discernibility.filter { it.count() == minDiscernibilityLength }.flatMap { it }
         val countInColumns = discernibility.flatMap { it }.groupBy { it }.mapValues { it.value.count() }
         return countInColumns.filter { candidates.contains(it.key) }.maxBy { it.value }!!.key
+    }
+
+    private fun removeRowsFromDiscernibility(discernibility: List<List<Int>>, argument: Int): List<List<Int>> {
+        return discernibility.filterNot { it.contains(argument) }
     }
 }
